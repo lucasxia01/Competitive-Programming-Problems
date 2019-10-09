5e81d39f2f0a35b6fa0e12eafbd6c57ef4bd6add
Lucas Xia <lucasxia01@gmail.com>
2019-10-08 21:39:02 -0700

    new contest Ed CF 74

--- Codeforces/NAQ Qualifer 19/NAQ Qualifer 19/L.cpp
+++ Codeforces/NAQ Qualifer 19/NAQ Qualifer 19/L.cpp
@@ -18,23 +18,11 @@ int main() {
     int n;
     cin >> n;
     int p[n][2];
-    F0R(i, n) {
-        cin >> p[i][0] >> p[i][1];
-    }
-    int ST[2][2][7][2*n];
+    F0R(i, n) cin >> p[i][0] >> p[i][1];
     int shift[7] = {0, 1, 2, 3, 2, 1, 0};
-    F0R(i, 7) {
-        F0R(dir, 2) {
-            F0R(k, 2)
-                F0R(j, n) {
-                    ST[dir][k][i][j+n] = p[j][0] + shift[((i+(2*dir-1)*j)%7+7)%7]*p[j][1];
-                }
-            F0Rd(j, n) {
-                ST[dir][0][i][j] = max(ST[dir][0][i][2*j], ST[dir][0][i][2*j+1]);
-                    ST[dir][1][i][j] = min(ST[dir][1][i][2*j], ST[dir][1][i][2*j+1]);
-            }
-        }
-    }
+    int SQRTN = sqrt(n);
+    int blocks = (n-SQRTN+1)/SQRTN;
+    
     int q, s, t;
     cin >> q;
     F0R(i, q) {
@@ -44,30 +32,9 @@ int main() {
         int day;
         if (s < t) {
             day = ((-s)%7+7)%7;
-            cout << day << endl;
-            int l = s, r = t+1;
-            for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
-                if (l&1) best = max(best, ST[1][0][day][l--]);
-                if (r&1) best = max(best, ST[1][0][day][--r]);
-            }
-            l = s; r = t+1;
-            for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
-                if (l&1) worst = min(worst, ST[1][1][day][l--]);
-                if (r&1) worst = min(worst, ST[1][1][day][--r]);
-            }
+            
         } else {
             day = s%7;
-            cout << day << endl;
-            int l = t, r = s+1;
-            for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
-                if (l&1) best = max(best, ST[0][0][day][l--]);
-                if (r&1) best = max(best, ST[0][0][day][--r]);
-            }
-            l = t; r = s+1;
-            for (l+=n, r+=n; l < r; l>>=1, r>>=1) {
-                if (l&1) worst = min(worst, ST[0][1][day][l--]);
-                if (r&1) worst = min(worst, ST[0][1][day][--r]);
-            }
         }
         cout << best << " " << worst << endl;
     }
