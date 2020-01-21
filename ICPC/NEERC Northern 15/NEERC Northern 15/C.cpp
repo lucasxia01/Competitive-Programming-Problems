cafd610040949638ffb00c2cbd9953217bbac47a
Lucas Xia <lucasxia01@gmail.com>
2020-01-21 01:10:05 -0800

    icpc training session 1

---
+++ ICPC/NEERC Northern 15/NEERC Northern 15/C.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <stdlib.h>
+#include <time.h>
+#include <complex>
+#include <iterator>
+#include <regex>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+#define sz(x) (int)x.size()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    freopen("concatenation.in", "r", stdin);
+    freopen("concatenation.out", "w", stdout);
+    string a, b;
+    cin >> a >> b;
+    ll ans = (ll) a.length() * b.length();
+    ll freqA[26] = {}, freqB[26] = {};
+    FOR(i, 1, (int) a.length()-1) {
+        freqA[a[i]-'a']++;
+    }
+    F0R(i, (int) b.length()-1) {
+        freqB[b[i]-'a']++;
+    }
+    F0R(i, 26) {
+        ans -= freqA[i]*freqB[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
