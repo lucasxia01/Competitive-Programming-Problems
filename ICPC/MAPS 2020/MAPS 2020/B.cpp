0b9f677cf149602fb7e6237f8b9517609e6a1de0
Lucas Xia <lucasxia01@gmail.com>
2020-03-29 00:49:25 -0700

    Mount Allison Contest, solved 8/12 as a team

--- ICPC/MAPS 2020/MAPS 2020/B.cpp
+++ ICPC/MAPS 2020/MAPS 2020/B.cpp
@@ -1,9 +1,95 @@
-//
-//  B.cpp
-//  MAPS 2020
-//
-//  Created by Lucas Xia on 3/28/20.
-//  Copyright © 2020 Lucas Xia. All rights reserved.
-//
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <stdlib.h>
+#include <time.h>
+#include <complex>
+#include <iterator>
+#include <regex>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
 
-#include <stdio.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+
+const int MAX_N = 100011;
+const int MX = 1<<20;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+#define sz(x) (int)x.size()
+
+template <typename T> bool ckmin(T& a, const T& b) {
+    return a > b ? a=b, 1 : 0;
+}
+template <typename T> bool ckmax(T& a, const T& b) {
+    return b > a ? a=b, 1 : 0;
+}
+ll b, d;
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%d;
+        a = (a * a)%d;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> b >> d;
+    b = b%d;
+    if (b == 0) {
+        cout << "no" << endl;
+        return 0;
+    } else if (d == 2) {
+        cout << "yes" << endl;
+        return 0;
+    }
+    ll q = d-1;
+    while (q%2==0) q/=2;
+    if (binpow(b,q) == 1) {
+        cout << "no" << endl;
+        return 0;
+    }
+    cout << "yes" << endl;
+}
