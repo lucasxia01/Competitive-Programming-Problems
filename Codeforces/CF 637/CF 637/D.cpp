b24beb79ef92c973fa534a2e19616b3fa365a81b
Lucas Xia <lucasxia01@gmail.com>
2020-04-29 16:55:49 -0700

    upsolved D Nastya and Time Machine

--- Codeforces/CF 637/CF 637/D.cpp
+++ Codeforces/CF 637/CF 637/D.cpp
@@ -51,6 +51,7 @@ typedef vector<pl> vpl;
 #define ub upper_bound
 #define sz(x) (int)x.size()
 
+const char nl = '\n';
 const int MAX_N = 100011;
 const ll INF = (1<<29) + 123;
 const ll MOD = 1000000007; // 998244353
@@ -65,14 +66,47 @@ template <typename T> bool ckmax(T& a, const T& b) {
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int MX = 1<<20;
+const int MX = 1e5+5;
+vi edges[MX];
+vpi ans;
+int T;
+
+void dfs(int v, int p, int t, int endTime) {
+    
+    int curT = t;
+    int childrenLeft = sz(edges[v])-1;
+    trav(u, edges[v]) {
+        if (u == p) continue;
+        ans.pb({v, curT++});
+        if (curT == T+1) {
+            // reset curT
+            curT = endTime-childrenLeft;
+            ans.pb({v, curT++});
+        }
+        dfs(u, v, curT, curT-1);
+        childrenLeft--;
+    }
+    ans.pb({v, curT});
+    if (curT != endTime) {
+        curT = endTime;
+        ans.pb({v, curT});
+    }
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int n;
+    int n, u, v;
     cin >> n;
-    int a[n];
-    F0R(i, n) cin >> a[i];
+    F0R(i, n-1) {
+        cin >> u >> v;
+        edges[u].pb(v);
+        edges[v].pb(u);
+    }
+    F0R(i, n) ckmax(T, sz(edges[i+1]));
+    dfs(1, 0, 0, sz(edges[1]));
+    cout << sz(ans) << nl;
+    trav(a, ans) cout << a.f << " " << a.s << nl;
     return 0;
 }
