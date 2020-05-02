b575c7a9377467ce812dc9d1c53f88e6e32e6812
Lucas Xia <lucasxia01@gmail.com>
2020-05-01 17:09:57 -0700

    upsolved old matching, ind set problem

--- Codeforces/CF 576/CF 576/E.cpp
+++ Codeforces/CF 576/CF 576/E.cpp
@@ -44,6 +44,7 @@ typedef vector<pl> vpl;
 #define lb lower_bound
 #define ub upper_bound
 
+const char nl = '\n';
 const int MAX_N = 1000011;
 const ll INF = (1<<29) + 123;
 const ll MOD = 1000000007; // 998244353
@@ -54,11 +55,39 @@ const ld PI = 4*atan((ld)1);
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n, k;
-    cin >> n >> k;
-    int a[n];
-    int count = 0;
-    F0R(i, n) cin >> a[i];
-    cout << count << endl;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, u, v;
+        cin >> n >> m;
+        // idea is that greedily find a matching, if its good enough, use it
+        // if not, then the remaining vertices all don't have edges between them, so
+        // they form an independent set
+        vpi edges;
+        F0R(i, m) {
+            cin >> u >> v;
+            edges.pb({u, v});
+        }
+        int vis[3*n+1];
+        memset(vis, 0, sizeof vis);
+        vi matching;
+        F0R(i, m) {
+            if (!vis[edges[i].f]&& !vis[edges[i].s]) {
+                matching.pb(i+1);
+                vis[edges[i].f]=vis[edges[i].s]=1;
+            }
+        }
+        if (sz(matching) >= n) {
+            cout << "Matching" << nl;
+            F0R(i, n) cout << matching[i] << " "; cout << nl;
+        } else {
+            cout << "IndSet" << nl;
+            int c = 0;
+            FOR(i, 1, 3*n) {
+                if (c == n) break;
+                if (!vis[i]) cout << i << " ", c++;
+            } cout << nl;
+        }
+    }
     return 0;
 }
