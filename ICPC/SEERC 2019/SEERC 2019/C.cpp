64330205394fca015f44860f841cf44265fc1b9f
Lucas Xia <lucasxia01@gmail.com>
2020-06-17 08:34:00 -0700

    upsolved SEERC 2019 C

--- ICPC/SEERC 2019/SEERC 2019/C.cpp
+++ ICPC/SEERC 2019/SEERC 2019/C.cpp
@@ -66,10 +66,12 @@ template <typename T> bool ckmax(T& a, const T& b) {
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+int n;
+
 int query2Range(int lo, int hi) {
     cout << "2 " << hi-lo+1;
-    FOR(i, lo, hi) cout << " " << i+1;
-    fflush(stdout);
+    FOR(i, lo, hi) cout << " " << i+1; cout << nl;
+    cout.flush();
     int count = (hi-lo)*(hi-lo+1)/2;
     int maxDiff = 0, x;
     F0R(i, count) {
@@ -81,14 +83,52 @@ int query2Range(int lo, int hi) {
 
 int query1(int p) {
     cout << "1 " << p+1 << nl;
+    cout.flush();
     int x; cin >> x;
     return x;
+}
+
+set<int> query2Bit(int bit, int m) {
+    vi idx;
+    FOR(i, 1, n) if (i&(1<<bit) && i != m) idx.pb(i);
+    int x; vi vals, allvals;
+    if (sz(idx) >= 2) {
+        cout << "2 " << sz(idx);
+        trav(a, idx) cout << " " << a; cout << nl;
+        cout.flush();
+        int count = sz(idx)*(sz(idx)-1)/2;
+        F0R(i, count) cin >> x, vals.pb(x);
+    }
+    idx.pb(m);
+    if (sz(idx) >= 2) {
+        cout << "2 " << sz(idx);
+        trav(a, idx) cout << " " << a; cout << nl;
+        cout.flush();
+        int count = sz(idx)*(sz(idx)-1)/2;
+        F0R(i, count) cin >> x, allvals.pb(x);
+    }
+    sort(vals.begin(), vals.end());
+    sort(allvals.begin(), allvals.end());
+    int j = 0;
+    set<int> v;
+    trav(a, allvals) {
+        if (j < sz(vals) && a == vals[j]) j++;
+        else v.insert(a);
+    }
+    return v;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int n; cin >> n;
+    cin >> n;
+    if (n <= 2) {
+        vi ans;
+        F0R(i, n) ans.pb(query1(i));
+        cout << "3";
+        trav(a, ans) cout << " " << a; cout << nl;
+        return 0;
+    }
     // binary search to find min/max
     int lo = 0, hi = n-1;
     int diff = query2Range(0, hi);
@@ -100,7 +140,26 @@ int main() {
     }
     int val = query1(hi);
     int val2 = query1(0); // for comparison
-    
-    
+    vector<set<int> > vals;
+    F0R(i, 8) vals.pb(query2Bit(i, hi+1));
+    cout << "3";
+    FOR(i, 1, n) {
+        if (i == hi+1) {
+            cout << " " << val;
+            continue;
+        }
+        map<int, int> m;
+        int ans = -1;
+        F0R(j, 8) {
+            if (i&(1<<j)) trav(a, vals[j]) m[a]++;
+            else trav(a, vals[j]) m[a]--;
+        }
+        trav(a, m) if (a.s == __builtin_popcount(i)) ans = a.f;
+        assert(ans != -1);
+        if (val2 <= val) cout << " " << val-ans;
+        else cout << " " << val+ans;
+    }
+    cout << nl;
+    cout.flush();
     return 0;
 }
