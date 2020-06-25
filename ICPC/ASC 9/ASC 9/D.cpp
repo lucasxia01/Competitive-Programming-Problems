3efe4f4a917dfea2b4447bec2bd609c07a457096
Lucas Xia <lucasxia01@gmail.com>
2020-06-25 10:12:05 -0700

    new contest ASC 9 and upsolving

--- ICPC/ASC 9/ASC 9/D.cpp
+++ ICPC/ASC 9/ASC 9/D.cpp
@@ -1,9 +1,127 @@
-//
-//  D.cpp
-//  ASC 9
-//
-//  Created by Lucas Xia on 6/24/20.
-//  Copyright © 2020 Lucas Xia. All rights reserved.
-//
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <stdlib.h>
+#include <time.h>
+#include <complex>
+#include <iterator>
+#include <regex>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <chrono>
+#include <cassert>
 
-#include <stdio.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+
+const char nl = '\n';
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) {
+    return a > b ? a=b, 1 : 0;
+}
+template <typename T> bool ckmax(T& a, const T& b) {
+    return b > a ? a=b, 1 : 0;
+}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 101;
+
+vi gr[MX];
+int vis[MX];
+vi order;
+void dfs1(int v) {
+    vis[v] = 1;
+    trav(u, gr[v]) {
+        if (vis[u]) continue;
+        dfs1(u);
+    }
+    order.pb(v);
+}
+int out, in, mark = 0, comp = 0;
+void dfs2(int v) {
+    vis[v] = mark;
+    comp++;
+    trav(u, gr[v]) {
+        if (vis[u]) continue;
+        dfs2(u);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+//    freopen("police.in", "r", stdin);
+//    freopen("police.out", "w", stdout);
+    int n, m, k; cin >> n >> m >> k;
+    int u, v;
+    F0R(i, m) {
+        cin >> u >> v;
+        gr[u].pb(v);
+    }
+    FOR(i, 1, n) if (!vis[i]) dfs1(i);
+    reverse(order.begin(), order.end());
+    memset(vis, 0, sizeof vis);
+    vi policeComps;
+    trav(a, order) {
+        if (!vis[a]) {
+            ++mark;
+            comp = 0; in = 0; out = 0;
+            dfs2(a);
+            if (!in || !out) policeComps.pb(comp);
+        }
+    }
+    int SZ = sz(policeComps);
+    __int128 dp[SZ+1][k+1]; F0R(i, SZ+1) F0R(j, k+1) dp[i][j] = 0;
+    dp[0][0] = 1;
+    F0R(i, SZ) {
+        F0R(j, k) {
+            FOR(l, 1, policeComps[i]) {
+                if (j+l > k) break;
+                dp[i+1][j+l] += dp[i][j];
+            }
+        }
+    }
+    return 0;
+}
--- ICPC/ASC 9/ASC 9/F.cpp
+++ ICPC/ASC 9/ASC 9/F.cpp
@@ -66,14 +66,89 @@ template <typename T> bool ckmax(T& a, const T& b) {
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int MX = 1<<20;
+const int MX = 205*205;
+
+vi edges[MX];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int n;
-    cin >> n;
-    int a[n];
-    F0R(i, n) cin >> a[i];
+//    freopen("restore.in", "r", stdin);
+//    freopen("restore.out", "w", stdout);
+    int n; cin >> n;
+    int a[n][n];
+    F0R(i, n) F0R(j, n) cin >> a[i][j];
+    int num_nodes = n;
+    int dist[n][MX]; F0R(i, n) F0R(j, MX) dist[i][j] = -1;
+    dist[0][0] = 0;
+    vpi edges;
+    vi gr[MX];
+    FOR(i, 1, n-1) {
+        // we currently have all distances up to i
+        int node = -1;
+        if (i == 1) node = 0;
+        else {
+            vi pos;
+            FOR(j, n, num_nodes-1) { // check if any node works between 0 and 1 to restruct search space
+                if (a[0][i]-dist[0][j]==a[1][i]-dist[1][j] && a[0][i]-dist[0][j]>0) pos.pb(j);
+            }
+            trav(p, pos) { // should be at most MX p
+                int d = a[0][i]-dist[0][p];
+                bool ok = 1;
+                F0R(j, i) {
+                    if (a[j][i]-dist[j][p]!=d) {
+                        ok = 0;
+                        break;
+                    }
+                }
+                if (ok) { node = p; break; }
+            }
+            if (node == -1) { // no possible node
+                cout << -1 << nl;
+                return 0;
+            }
+        }
+        if (a[0][i]-dist[0][node] == 1) {
+            F0R(j, i) dist[j][i] = dist[j][node]+1;
+            gr[i].pb(node);
+            gr[node].pb(i);
+            if (i < node) edges.pb({i, node});
+            else edges.pb({node, i});
+        } else {
+            F0R(j, i) dist[j][num_nodes] = dist[j][node]+1;
+            edges.pb({node, num_nodes});
+            gr[node].pb(num_nodes);
+            gr[num_nodes].pb(node);
+            F0R(j, a[0][i]-dist[0][node]-2) {
+                F0R(jj, i) dist[jj][num_nodes+1] = dist[jj][num_nodes]+1;
+                gr[num_nodes+1].pb(num_nodes);
+                gr[num_nodes].pb(num_nodes+1);
+                edges.pb({num_nodes, num_nodes+1});
+                num_nodes++;
+            }
+            F0R(j, i) dist[j][i] = dist[j][num_nodes]+1;
+            gr[i].pb(num_nodes);
+            gr[num_nodes].pb(i);
+            edges.pb({i, num_nodes++});
+        }
+        queue<int> q; q.push(i); // setting distances from i to everything else in the graph
+        bool vis[num_nodes]; F0R(j, num_nodes) vis[j] = 0;
+        vis[i] = 1;
+        dist[i][i] = 0;
+        while (!q.empty()) {
+            int cur = q.front(); q.pop();
+            trav(v, gr[cur]) {
+                if (vis[v]) continue;
+                vis[v] = 1;
+                dist[i][v] = dist[i][cur]+1;
+                q.push(v);
+            }
+        }
+    }
+    
+    sort(edges.begin(), edges.end());
+    assert(num_nodes==sz(edges)+1);
+    cout << num_nodes << nl;
+    trav(e, edges) cout << e.f+1 << " " << e.s+1 << nl;
     return 0;
 }
