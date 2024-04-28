76349ee6cad6abf452e0d60a1cbe45126829a317
Lucas Xia <lucasxia01@gmail.com>
2024-04-28 13:31:59 -0400

    upsolve E from CF 941

---
+++ Codeforces/CF 941/E.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) { return a > b ? a=b, 1 : 0; }
+template <typename T> bool ckmax(T& a, const T& b) { return b > a ? a=b, 1 : 0; }
+
+/**** Credit to chatgpt 4.0 ****/
+
+// Stream operator for std::pair
+template<typename T1, typename T2>
+ostream& operator<<(ostream &out, const pair<T1, T2> &v) {
+    out << "(" << v.first << ", " << v.second << ")"; 
+    return out;
+}
+
+// Trait to check if a type is iterable
+template<typename T, typename = void>
+struct is_iterable : false_type {};
+
+template<typename T>
+struct is_iterable<T, void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))>> : true_type {};
+
+// Stream operator for iterable types excluding std::string
+template<typename TT>
+typename enable_if<is_iterable<TT>::value && !is_same<TT, string>::value, ostream&>::type
+operator<<(ostream& out, const TT& c) {
+    out << "{ ";
+    for (const auto& x : c) out << x << " ";
+    out << "}"; 
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::stack<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.top());
+        container.pop();
+    }
+    std::reverse(elements.begin(), elements.end()); // Reverse to maintain order
+    return out << elements;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::queue<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.front());
+        container.pop();
+    }
+    return out << elements;
+}
+
+// Helper function to print std::priority_queue
+template<typename T, typename Container, typename Compare>
+ostream& operator<<(ostream& out, std::priority_queue<T, Container, Compare> pq) {
+    out << "{";
+    while (!pq.empty()) {
+        out << " " << pq.top();
+        pq.pop();
+    }
+    out << " }";
+    return out;
+}
+
+#ifdef DBG
+void dbg_out() { cerr << endl; }
+
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+#define dbg(...) cerr << #__VA_ARGS__ << ":", dbg_out(__VA_ARGS__);
+#define dbg_array(a, n) cerr << #a << ": { "; for(int i = 0; i < n; i++) cerr << a[i] << " "; cerr << "}\n";
+#else
+#define dbg(...)
+#define dbg_array(a, n)
+#endif
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 3e5+5;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n, m, k; cin >> n >> m >> k;
+    int c[n][m]; F0R(i, n) F0R(j, m) cin >> c[i][j];
+    vector<array<int, 4>> ans;
+    F0R(j, m) {
+        F0R(i, n) {
+            // go up by n-1-i
+            F0R(z, n-1-i) ans.pb({i, j, z+1, c[i][j]});
+            // go down by i
+            F0R(x, i) ans.pb({i+1+x, j, n-1-i, c[i][j]});
+            // now go up again by n+k-(n-i)+1
+            F0R(z, k+i+2) ans.pb({2*i, j, n-i+z, c[i][j]});
+        }
+    }
+    int base = n+2; // high is n+k+1
+    F0R(z, k) {
+        // fill in column m with the color
+        F0R(i, 2*n) ans.pb({i, m, base+z, z+1});
+        // fill in every other row with the color
+        // so row 1, 5, 9, 13, ...
+        int i = 1;
+        while (i < 2*n) {
+            F0R(j, m) ans.pb({i, j, base+z, z+1});
+            i += 4;
+        }
+    }
+    assert(sz(ans) < (int)4e5);
+    cout << sz(ans) << nl;
+    trav(x, ans) {
+        cout << x[0]+1 << " " << x[1]+1 << " " << x[2]+1 << " " << x[3] << nl;
+    }
+    return 0;
+}
