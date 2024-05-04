83dabeb2edfccfbe85d8c4fa08bc574da78d3cd9
Lucas Xia <lucasxia01@gmail.com>
2024-05-03 20:47:51 -0400

    Ed CF 165 E

---
+++ Codeforces/Ed CF 165/E.cpp
@@ -0,0 +1,205 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) { return a > b ? a=b, 1 : 0; }
+template <typename T> bool ckmax(T& a, const T& b) { return b > a ? a=b, 1 : 0; }
+
+/**** Credit to chatgpt 4.0 ****/
+
+// Stream operator for std::pair
+template<typename T1, typename T2>
+ostream& operator<<(ostream &out, const pair<T1, T2> &v) {
+    out << "(" << v.first << ", " << v.second << ")"; 
+    return out;
+}
+
+// Trait to check if a type is iterable
+template<typename T, typename = void>
+struct is_iterable : false_type {};
+
+template<typename T>
+struct is_iterable<T, void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))>> : true_type {};
+
+// Stream operator for iterable types excluding std::string
+template<typename TT>
+typename enable_if<is_iterable<TT>::value && !is_same<TT, string>::value, ostream&>::type
+operator<<(ostream& out, const TT& c) {
+    out << "{ ";
+    for (const auto& x : c) out << x << " ";
+    out << "}"; 
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::stack<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.top());
+        container.pop();
+    }
+    std::reverse(elements.begin(), elements.end()); // Reverse to maintain order
+    return out << elements;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::queue<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.front());
+        container.pop();
+    }
+    return out << elements;
+}
+
+// Helper function to print std::priority_queue
+template<typename T, typename Container, typename Compare>
+ostream& operator<<(ostream& out, std::priority_queue<T, Container, Compare> pq) {
+    out << "{";
+    while (!pq.empty()) {
+        out << " " << pq.top();
+        pq.pop();
+    }
+    out << " }";
+    return out;
+}
+
+#ifdef DBG
+void dbg_out() { cerr << endl; }
+
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+#define dbg(...) cerr << #__VA_ARGS__ << ":", dbg_out(__VA_ARGS__);
+#define dbg_array(a, n) cerr << #a << ": { "; for(int i = 0; i < n; i++) cerr << a[i] << " "; cerr << "}\n";
+#else
+#define dbg(...)
+#define dbg_array(a, n)
+#endif
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 3e5+5;
+
+
+const int inf = -1e9;
+struct Node {
+	Node *l = 0, *r = 0;
+	int lo, hi, mset = inf, madd = 0, val = 0;
+	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of 0
+	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+		if (lo + 1 < hi) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
+			val = min(l->val, r->val);
+		}
+		else val = v[lo];
+	}
+	int query(int L, int R) {
+		if (R <= lo || hi <= L) return -inf;
+		if (L <= lo && hi <= R) return val;
+		push();
+		return min(l->query(L, R), r->query(L, R));
+	}
+	void set(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) mset = val = x, madd = 0;
+		else {
+			push(), l->set(L, R, x), r->set(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void add(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) {
+			if (mset != inf) mset += x;
+			else madd += x;
+			val += x;
+		}
+		else {
+			push(), l->add(L, R, x), r->add(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void push() {
+		if (!l) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(lo, mid); r = new Node(mid, hi);
+		}
+		if (mset != inf)
+			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
+		else if (madd)
+			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+	}
+};
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int a[n]; F0R(i, n) cin >> a[i];
+        int ans = 0;
+        Node st(0, n);
+        int last[n+1]; F0R(i, n+1) last[i] = -1;
+        int last2[n+1]; F0R(i, n+1) last2[i] = -1;
+        int prev = -1;
+        F0R(i, n) {
+            // set [last[a[i]]+1, i]
+            if (last[a[i]] != -1) {
+                dbg("decrement", last2[a[i]]+1, last[a[i]]);
+                st.add(last2[a[i]]+1, last[a[i]]+1, -1);
+            }
+            st.add(last[a[i]]+1, i+1, 1);
+            dbg("increment", last[a[i]]+1, i);
+            last2[a[i]] = last[a[i]];
+            last[a[i]] = i;
+            dbg(i, st.query(prev+1, i+1));
+            if (st.query(prev+1, i+1) == 0) {
+                // we need to set this to 0
+                ans++;
+                prev = i;
+                st.set(0, n, 0);
+            }
+        }
+        cout << ans << nl;
+    }
+    return 0;
+}
