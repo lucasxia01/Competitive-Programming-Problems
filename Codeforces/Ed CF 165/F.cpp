70609482a31ef6cd7fd11136b64693163450cca1
Lucas Xia <lucasxia01@gmail.com>
2024-05-11 22:11:11 -0400

    failed upsolve attempt

---
+++ Codeforces/Ed CF 165/F.cpp
@@ -0,0 +1,221 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 1000000007; // 998244353
+const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) { return a > b ? a=b, 1 : 0; }
+template <typename T> bool ckmax(T& a, const T& b) { return b > a ? a=b, 1 : 0; }
+
+/**** Credit to chatgpt 4.0 ****/
+
+// Stream operator for std::pair
+template<typename T1, typename T2>
+ostream& operator<<(ostream &out, const pair<T1, T2> &v) {
+    out << "(" << v.first << ", " << v.second << ")"; 
+    return out;
+}
+
+// Trait to check if a type is iterable
+template<typename T, typename = void>
+struct is_iterable : false_type {};
+
+template<typename T>
+struct is_iterable<T, void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))>> : true_type {};
+
+// Stream operator for iterable types excluding std::string
+template<typename TT>
+typename enable_if<is_iterable<TT>::value && !is_same<TT, string>::value, ostream&>::type
+operator<<(ostream& out, const TT& c) {
+    out << "{ ";
+    for (const auto& x : c) out << x << " ";
+    out << "}"; 
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::stack<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.top());
+        container.pop();
+    }
+    std::reverse(elements.begin(), elements.end()); // Reverse to maintain order
+    return out << elements;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::queue<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.front());
+        container.pop();
+    }
+    return out << elements;
+}
+
+// Helper function to print std::priority_queue
+template<typename T, typename Container, typename Compare>
+ostream& operator<<(ostream& out, std::priority_queue<T, Container, Compare> pq) {
+    out << "{";
+    while (!pq.empty()) {
+        out << " " << pq.top();
+        pq.pop();
+    }
+    out << " }";
+    return out;
+}
+
+#ifdef DBG
+void dbg_out() { cerr << endl; }
+
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+#define dbg(...) cerr << #__VA_ARGS__ << ":", dbg_out(__VA_ARGS__);
+#define dbg_array(a, n) cerr << #a << ": { "; for(int i = 0; i < n; i++) cerr << a[i] << " "; cerr << "}\n";
+#else
+#define dbg(...)
+#define dbg_array(a, n)
+#endif
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 3e5+5;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n, k; cin >> n >> k;
+    int a[n]; F0R(i, n) {
+        cin >> a[i];
+        a[i]--;
+    }
+    // F0R(i, 200) cout << i+1 << ' ';
+    // F0R(i, 800) cout << 1 << ' '; cout << nl;
+    // now just greedy
+    vector<bool> curS(k, 0);
+    int cnt = 0;
+    int i = 0;
+    while (i < k) {
+        curS[a[i]] = !curS[a[i]];
+        if (curS[a[i]]) cnt++;
+        else cnt--;
+        i++;
+    }
+    while (i < n && cnt < k) {
+        curS[a[i]] = !curS[a[i]];
+        if (curS[a[i]]) cnt++;
+        else cnt--;
+        i++;
+    }
+    dbg("first", i);
+    int ans = 0;
+    int rem = 0;
+    if (i == n) rem = cnt;
+    F0R(j, k) curS[j] = 1;
+    cnt = k;
+    while (i < n) {
+        assert (cnt == k);
+        ans++;
+        // we're saturated and need to throw out two
+        // just try all kC2 choices
+        int hi = -1;
+        F0R(j, k) {
+            F0R(l, j) {
+                // j and l are removed
+                int ii = i;
+                curS[j] = 0;
+                curS[l] = 0;
+                cnt--;
+                cnt--;
+                vi other;
+                while (ii < n && cnt < k) {
+                    curS[a[ii]] = !curS[a[ii]];
+                    if (curS[a[ii]]) cnt++;
+                    else cnt--;
+                    other.pb(a[ii]);
+                    ii++;
+                }
+                ckmax(hi, ii);
+                trav(o, other) curS[o] = 1;
+                curS[l] = 1;
+                curS[j] = 1;
+                cnt = k;
+            }
+        }
+        if (hi == n) { // we need to maximize the amount remaining instead
+            F0R(j, k) curS[j] = 1;
+            cnt = k;
+            int lo = k+1;
+            F0R(j, k) {
+                F0R(l, j) {
+                    // j and l are removed
+                    int ii = i;
+                    curS[j] = 0;
+                    curS[l] = 0;
+                    cnt--;
+                    cnt--;
+                    vi other;
+                    while (ii < n && cnt < k) {
+                        curS[a[ii]] = !curS[a[ii]];
+                        if (curS[a[ii]]) cnt++;
+                        else cnt--;
+                        other.pb(a[ii]);
+                        ii++;
+                    }
+                    if (ii == n) {
+                        // dbg(l, j, cnt, curS);
+                        ckmin(lo, cnt);
+                    }
+                    trav(o, other) curS[o] = 1;
+                    curS[l] = 1;
+                    curS[j] = 1;
+                    cnt = k;
+                }
+            }
+            rem = lo;
+        }
+        dbg(i, hi);
+        i = hi;
+    }
+    ans += rem/2;
+    dbg(ans, rem);
+    cout << n/2-ans << nl;
+    return 0;
+}
