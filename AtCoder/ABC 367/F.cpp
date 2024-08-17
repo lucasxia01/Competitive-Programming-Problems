71131a574dd4434a8f5be4a8c368b266305ce7c4
Lucas Xia <lucasxia01@gmail.com>
2024-08-17 17:57:52 -0400

    ABC 367

---
+++ AtCoder/ABC 367/F.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int ll
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+const int MAX_N = 100011;
+const ll INF = (1<<29) + 123;
+const ll MOD = 173227498367539991;
+const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) { return a > b ? a=b, 1 : 0; }
+template <typename T> bool ckmax(T& a, const T& b) { return b > a ? a=b, 1 : 0; }
+
+/**** Credit to chatgpt 4.0 ****/
+
+// Stream operator for std::pair
+template<typename T1, typename T2>
+ostream& operator<<(ostream &out, const pair<T1, T2> &v) {
+    out << "(" << v.first << ", " << v.second << ")"; 
+    return out;
+}
+
+// Trait to check if a type is iterable
+template<typename T, typename = void>
+struct is_iterable : false_type {};
+
+template<typename T>
+struct is_iterable<T, void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))>> : true_type {};
+
+// Stream operator for iterable types excluding std::string
+template<typename TT>
+typename enable_if<is_iterable<TT>::value && !is_same<TT, string>::value, ostream&>::type
+operator<<(ostream& out, const TT& c) {
+    out << "{ ";
+    for (const auto& x : c) out << x << " ";
+    out << "}"; 
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::stack<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.top());
+        container.pop();
+    }
+    std::reverse(elements.begin(), elements.end()); // Reverse to maintain order
+    return out << elements;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, std::queue<T> container) {
+    std::vector<T> elements;
+    while (!container.empty()) {
+        elements.push_back(container.front());
+        container.pop();
+    }
+    return out << elements;
+}
+
+// Helper function to print std::priority_queue
+template<typename T, typename Container, typename Compare>
+ostream& operator<<(ostream& out, std::priority_queue<T, Container, Compare> pq) {
+    out << "{";
+    while (!pq.empty()) {
+        out << " " << pq.top();
+        pq.pop();
+    }
+    out << " }";
+    return out;
+}
+
+#ifdef DBG
+void dbg_out() { cerr << endl; }
+
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+
+#define dbg(...) cerr << #__VA_ARGS__ << ":", dbg_out(__VA_ARGS__);
+#define dbg_array(a, n) cerr << #a << ": { "; for(int i = 0; i < n; i++) cerr << a[i] << " "; cerr << "}\n";
+#else
+#define dbg(...)
+#define dbg_array(a, n)
+#endif
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MX = 20*(2e5+5);
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    // precompute powers of 2 mod MOD up to MX
+    int pow2[MX]; pow2[0] = 1;
+    F0R(i, MX-1) pow2[i+1] = (pow2[i]*2)%MOD;
+    int n, q; cin >> n >> q;
+    int a[n]; F0R(i, n) cin >> a[i];
+    int b[n]; F0R(i, n) cin >> b[i];
+    // calculate a prefix sum for each
+    int prefa[n+1]; prefa[0] = 0;
+    F0R(i, n) prefa[i+1] = prefa[i] + pow2[20*a[i]];
+    int prefb[n+1]; prefb[0] = 0;
+    F0R(i, n) prefb[i+1] = prefb[i] + pow2[20*b[i]];
+    while (q--) {
+        int l1, r1, L1, R1; cin >> l1 >> r1 >> L1 >> R1;
+        l1--; L1--;
+        if (prefa[r1]-prefa[l1] == prefb[R1]-prefb[L1]) cout << "Yes\n";
+        else cout << "No\n";
+    }
+    return 0;
+}
