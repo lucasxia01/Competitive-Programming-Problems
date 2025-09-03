25eec166e74587f08a4dd81d23782fc0d29e138e
Lucas Xia <lucasxia01@gmail.com>
2025-09-03 10:25:43 -0400

    dcc A-D

---
+++ DMOJ/Dnialh Contest 1/B.cpp
@@ -0,0 +1,299 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define int ll
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define F0R(i,n) for (int i = 0; i < n; i++)
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define FORd(i,a,b) for (int i = (b); i >= (a); i--)
+#define trav(a, x) for (auto& a : x)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+const char nl = '\n';
+// const ll INF = (1LL<<55) + 123;
+// const ll MOD = 998244353; // 1000000007;
+// const ld PI = 4*atan((ld)1);
+
+template <typename T> bool ckmin(T& a, const T& b) { return a > b ? a=b, 1 : 0; }
+template <typename T> bool ckmax(T& a, const T& b) { return b > a ? a=b, 1 : 0; }
+
+#ifdef DBG
+#include "/Users/lucasxia/Competitive-Programming-Problems/Library/debug.hpp"
+#else
+#define dbg(...)
+#define dbg_array(a, n)
+#endif
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// const int MX = 3e5+5;
+
+template <typename T>
+T inverse(T a, T m) {
+  T u = 0, v = 1;
+  while (a != 0) {
+    T t = m / a;
+    m -= t * a; swap(a, m);
+    u -= t * v; swap(u, v);
+  }
+  assert(m == 1);
+  return u;
+}
+
+template <typename T>
+class Modular {
+ public:
+  using Type = typename decay<decltype(T::value)>::type;
+
+  constexpr Modular() : value() {}
+  template <typename U>
+  Modular(const U& x) {
+    value = normalize(x);
+  }
+
+  template <typename U>
+  static Type normalize(const U& x) {
+    Type v;
+    if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+    else v = static_cast<Type>(x % mod());
+    if (v < 0) v += mod();
+    return v;
+  }
+
+  const Type& operator()() const { return value; }
+  template <typename U>
+  explicit operator U() const { return static_cast<U>(value); }
+  constexpr static Type mod() { return T::value; }
+
+  Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+  Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+  template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+  template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+  Modular& operator++() { return *this += 1; }
+  Modular& operator--() { return *this -= 1; }
+  Modular operator++(int32_t) { Modular result(*this); *this += 1; return result; }
+  Modular operator--(int32_t) { Modular result(*this); *this -= 1; return result; }
+  Modular operator-() const { return Modular(-value); }
+
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+#ifdef _WIN32
+    uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+    uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+    asm(
+      "divl %4; \n\t"
+      : "=a" (d), "=d" (m)
+      : "d" (xh), "a" (xl), "r" (mod())
+    );
+    value = m;
+#else
+    value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+    int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+    value = normalize(value * rhs.value - q * mod());
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+    value = normalize(value * rhs.value);
+    return *this;
+  }
+
+  Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+
+  template <typename U>
+  friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+
+  template <typename U>
+  friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+
+  template <typename U>
+  friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
+
+ private:
+  Type value;
+};
+
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+  assert(b >= 0);
+  Modular<T> x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+  }
+  return res;
+}
+
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+  return stream << number();
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+  typename common_type<typename Modular<T>::Type, int64_t>::type x;
+  stream >> x;
+  number.value = Modular<T>::normalize(x);
+  return stream;
+}
+
+/*
+using ModType = int;
+struct VarMod { static ModType value; };
+ModType VarMod::value;
+ModType& md = VarMod::value;
+using Mint = Modular<VarMod>;
+*/
+
+constexpr int md = 998244353;
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+// find integer sqrt
+int find_sqrt(int x) {
+    int y = sqrt(x);
+    while (y*y > x) y--;
+    while (y*y <= x) y++;
+    y--;
+    return y;
+}
+
+Mint sum_squares(Mint x) {
+    return x*(x+1)*(Mint(2)*x+1) / Mint(6);
+}
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        int a, b; cin >> a >> b;
+        if (a > b) swap(a, b); // so a <= b
+        Mint A = a;
+        Mint B = b;
+        // find the largest square under a
+        Mint x = find_sqrt(a);
+        // find the sum of these squares
+        Mint ans = sum_squares(x);
+        ans += x;
+        // for between A and B
+        Mint y = find_sqrt(b);
+        // we add a+1 for each one
+        ans += (A+1)*(y-x);
+        // for between B and A+B
+        Mint z = find_sqrt(a+b);
+        // we add A+B+1-i for each one, where i is the sq
+        ans += (A+B+1)*(z-y);
+        ans -= sum_squares(z)-sum_squares(y);
+        cout << ans+1 << nl;
+    }
+    return 0;
+}
+
+/*
+Basic Debugging:
+n = 0, 1, 2, MAX?
+Generate max cases, especially if simple to generate.
+memory usage?
+OVERFLOW?
+
+Wrong answer:
+Print your solution! Print debug output, as well.
+Are you clearing all data structures between test cases?
+Can your algorithm handle the whole range of input?
+Read the full problem statement again.
+Do you handle all corner cases correctly?
+Have you understood the problem correctly?
+Any uninitialized variables?
+Any overflows?
+Confusing N and M, i and j, etc.?
+Are you sure your algorithm works?
+What special cases have you not thought of?
+Are you sure the STL functions you use work as you think?
+Add some assertions, maybe resubmit.
+Create some testcases to run your algorithm on.
+Go through the algorithm for a simple case.
+Go through this list again.
+Explain your algorithm to a teammate.
+Ask the teammate to look at your code.
+Go for a small walk, e.g. to the toilet.
+Is your output format correct? (including whitespace)
+Rewrite your solution from the start or let a teammate do it.
+
+Runtime error:
+Have you tested all corner cases locally?
+Any uninitialized variables?
+Are you reading or writing outside the range of any vector?
+Any assertions that might fail?
+Any possible division by 0? (mod 0 for example)
+Any possible infinite recursion?
+Invalidated pointers or iterators?
+Are you using too much memory?
+Debug with resubmits (e.g. remapped signals, see Various).
+
+Time limit exceeded:
+Do you have any possible infinite loops?
+What is the complexity of your algorithm?
+Are you copying a lot of unnecessary data? (References)
+How big is the input and output? (consider scanf)
+Avoid vector, map. (use arrays/unordered_map)
+What do your teammates think about your algorithm?
+
+Memory limit exceeded:
+What is the max amount of memory your algorithm should need?
+Are you clearing all data structures between test cases?
+*/
